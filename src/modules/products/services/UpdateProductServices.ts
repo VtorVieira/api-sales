e11ecd54f8c1036c0def25d84057c570a137cb29
VtorVieira <vitor.vieira@digitalrepublic.com.br>
2024-04-11 21:51:50 -0300

    feat: create update product

---
+++ src/modules/products/services/UpdateProductServices.ts
@@ -0,0 +1,43 @@
+import { Product } from '../typeorm/entities/Product';
+import { ProductRepository } from '../typeorm/repositories/ProductRepository';
+import AppError from '@shared/errors/AppError';
+
+interface IRequest {
+  id: string;
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+export default class UpdateProductService {
+  public async execute({
+    id,
+    name,
+    price,
+    quantity
+  }: IRequest): Promise<Product> {
+    const product = await ProductRepository.findOne({
+      where: {
+        id
+      }
+    });
+
+    if (!product) {
+      throw new AppError('Product not found');
+    }
+
+    const productExists = await ProductRepository.findByName(name);
+
+    if (productExists && name !== product.name) {
+      throw new AppError('There is already one product with this name');
+    }
+
+    product.name = name;
+    product.price = price;
+    product.quantity = quantity;
+
+    await ProductRepository.save(product);
+
+    return product;
+  }
+}

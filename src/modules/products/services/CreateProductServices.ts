451a38480beb86a56eed313c64fc41fbaede339d
VtorVieira <vitor.vieira@digitalrepublic.com.br>
2024-04-11 21:12:30 -0300

    feat: create product service

---
+++ src/modules/products/services/CreateProductServices.ts
@@ -0,0 +1,29 @@
+import { Product } from '../typeorm/entities/Product';
+import { ProductRepository } from '../typeorm/repositories/ProductRepository';
+import AppError from '@shared/errors/AppError';
+
+interface IRequest {
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+export default class CreateProductService {
+  public async execute({ name, price, quantity }: IRequest): Promise<Product> {
+    const productExists = await ProductRepository.findByName(name);
+
+    if (productExists) {
+      throw new AppError('There is already one product with this name');
+    }
+
+    const product = ProductRepository.create({
+      name,
+      price,
+      quantity
+    });
+
+    await ProductRepository.save(product);
+
+    return product;
+  }
+}

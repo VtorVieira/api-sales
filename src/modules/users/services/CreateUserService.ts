7759581370fbdbe7468f2ea192c96a4e8481ee80
VtorVieira <vitor.vieira@digitalrepublic.com.br>
2024-05-08 08:15:52 -0300

    feat: add hashed password on create user

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -1,3 +1,4 @@
+import { hash } from 'bcryptjs';
 import { getCustomRepository } from 'typeorm';
 import AppError from '@shared/errors/AppError';
 import UserRespository from '../typeorm/repositories/UsersRespository';
@@ -18,10 +19,12 @@ export default class CreateUserService {
       throw new AppError('Email address already used.');
     }
 
+    const hashedPassword = await hash(password, 8);
+
     const user = usersRepository.create({
       name,
       email,
-      password
+      password: hashedPassword
     });
 
     await usersRepository.save(user);

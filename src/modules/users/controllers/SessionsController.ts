4e7abb3ebad10c4638ec95f1b5c445faad41e659
VtorVieira <vitor.vieira@digitalrepublic.com.br>
2024-05-08 08:34:44 -0300

    feat: create session controller

---
+++ src/modules/users/controllers/SessionsController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+import CreateSessionService from '../services/CreateSessionService';
+
+export default class SessionsController {
+  public async create(req: Request, res: Response): Promise<Response> {
+    const { email, password } = req.body;
+
+    const createUser = new CreateSessionService();
+
+    const user = await createUser.execute({
+      email,
+      password
+    });
+
+    return res.json(user);
+  }
+}

6e9fa05fb73e8a07253d6bed56e69e0d3677b9da
VtorVieira <vitor.vieira@digitalrepublic.com.br>
2024-05-12 12:38:38 -0300

    feat: add sign token create session

--- src/modules/users/services/CreateSessionService.ts
+++ src/modules/users/services/CreateSessionService.ts
@@ -3,14 +3,20 @@ import { getCustomRepository } from 'typeorm';
 import AppError from '@shared/errors/AppError';
 import UserRespository from '../typeorm/repositories/UsersRespository';
 import User from '../typeorm/entities/User';
+import { sign } from 'jsonwebtoken';
 
 interface IRequest {
   email: string;
   password: string;
 }
 
+interface IResponse {
+  user: User;
+  token: string;
+}
+
 export default class CreateSessionService {
-  public async execute({ email, password }: IRequest): Promise<User> {
+  public async execute({ email, password }: IRequest): Promise<IResponse> {
     const usersRepository = getCustomRepository(UserRespository);
     const user = await usersRepository.findByEmail(email);
 
@@ -24,6 +30,11 @@ export default class CreateSessionService {
       throw new AppError('Email or Passwor invalid.', 401);
     }
 
-    return user;
+    const token = sign({}, '5393b611eb971108e2a6e470ca1679f7', {
+      subject: user.id,
+      expiresIn: '1d'
+    });
+
+    return { user, token };
   }
 }

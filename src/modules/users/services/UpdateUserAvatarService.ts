52006fe8ac641c008222e1ecbad8bafaca9e52ec
VtorVieira <vitor.vieira@digitalrepublic.com.br>
2024-05-19 18:50:21 -0300

    feat: create upload user avatar service

---
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -0,0 +1,39 @@
+import { getCustomRepository } from 'typeorm';
+import path from 'path';
+import fs from 'fs';
+import AppError from '@shared/errors/AppError';
+import UserRespository from '../typeorm/repositories/UsersRespository';
+import User from '../typeorm/entities/User';
+import uploadConfig from '@config/upload';
+
+interface IRequest {
+  user_id: string;
+  avatarFileName: string;
+}
+
+export default class UpdateUserAvatarService {
+  public async execute({ user_id, avatarFileName }: IRequest): Promise<User> {
+    const usersRepository = getCustomRepository(UserRespository);
+
+    const user = await usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError('User not found.');
+    }
+
+    if (user.avatar) {
+      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
+      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
+
+      if (userAvatarFileExists) {
+        await fs.promises.unlink(userAvatarFilePath);
+      }
+    }
+
+    user.avatar = avatarFileName;
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}

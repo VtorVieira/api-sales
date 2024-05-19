008e3c39f439be4ea1c71da76132107c7339e963
VtorVieira <vitor.vieira@digitalrepublic.com.br>
2024-05-19 19:37:50 -0300

    feat: create send forgot service

---
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,24 @@
+import { getCustomRepository } from 'typeorm';
+import AppError from '@shared/errors/AppError';
+import UserRespository from '../typeorm/repositories/UsersRespository';
+import UserTokensRespository from '../typeorm/repositories/UserTokenRespository';
+
+interface IRequest {
+  email: string;
+}
+
+export default class SendForgotPasswordEmailService {
+  public async execute({ email }: IRequest): Promise<void> {
+    const usersRepository = getCustomRepository(UserRespository);
+    const userTokenRepository = getCustomRepository(UserTokensRespository);
+    const user = await usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('User does not exist.');
+    }
+
+    await userTokenRepository.generate(user.id);
+
+    console.log('Token');
+  }
+}

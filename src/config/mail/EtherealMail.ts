c91d4d613eb8eda1a055ff6891d53dd85b5ccc4c
VtorVieira <vitor.vieira@digitalrepublic.com.br>
2024-05-19 21:10:11 -0300

    refactor: add interface to send email

--- src/config/mail/EtherealMail.ts
+++ src/config/mail/EtherealMail.ts
@@ -1,13 +1,37 @@
 import nodemailer from 'nodemailer';
+import HandlebarsMailTemplate from './HandlebarsMailTemplate';
 
+interface ITemplateVariable {
+  [key: string]: string | number;
+}
+
+interface IParseMailTemplate {
+  template: string;
+  variables: ITemplateVariable;
+}
+
+interface IMailContact {
+  name: string;
+  email: string;
+}
 interface ISendEmail {
-  to: string;
-  body: string;
+  to: IMailContact;
+  from?: IMailContact;
+  subject: string;
+  templateData: IParseMailTemplate;
 }
 
 export default class EtherealMail {
-  static async sendMail({ to, body }: ISendEmail): Promise<void> {
+  static async sendMail({
+    to,
+    from,
+    subject,
+    templateData
+  }: ISendEmail): Promise<void> {
     const account = await nodemailer.createTestAccount();
+    const mailTemplate = new HandlebarsMailTemplate();
+
+    console.log('account', account);
 
     const transporter = nodemailer.createTransport({
       host: account.smtp.host,
@@ -20,10 +44,16 @@ export default class EtherealMail {
     });
 
     const message = await transporter.sendMail({
-      from: 'equipe@sales.com.br',
-      to,
-      subject: 'Recuperar senha',
-      text: body
+      from: {
+        name: from?.name || 'Sale Team',
+        address: from?.email || 'equipe@sales.com.br'
+      },
+      to: {
+        name: to.name,
+        address: to.email
+      },
+      subject,
+      html: await mailTemplate.parse(templateData)
     });
 
     console.log('Message sent: %s', message.messageId);

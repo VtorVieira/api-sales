0fc3ed0e4afdaec844120e7d12bc473608e2f565
VtorVieira <vitor.vieira@digitalrepublic.com.br>
2024-06-01 20:09:53 -0300

    feat: create order service

---
+++ src/modules/orders/services/CreateOrderService.ts
@@ -0,0 +1,88 @@
+import { getCustomRepository } from 'typeorm';
+import AppError from '@shared/errors/AppError';
+import Order from '../typeorm/entities/Order';
+import OrdersRepository from '../repositories/OrdersRepository';
+import Customer from '@modules/customers/typeorm/entities/Customer';
+import CustomersRespository from '@modules/customers/repositories/CustomersRepository';
+import ProductRepository from '@modules/products/typeorm/repositories/ProductRepository';
+
+interface IProduct {
+  id: string;
+  quantity: number;
+}
+
+interface IRequest {
+  customer: Customer;
+  products: IProduct[];
+}
+export default class CreateOrderService {
+  public async execute({ customer, products }: IRequest): Promise<Order> {
+    const ordersRepository = getCustomRepository(OrdersRepository);
+    const customersRepository = getCustomRepository(CustomersRespository);
+    const productsRepository = getCustomRepository(ProductRepository);
+
+    const customerExists = await customersRepository.findById(customer.id);
+
+    if (!customerExists) {
+      throw new AppError('Could not find any customer with this id.');
+    }
+
+    const existsProducts = await productsRepository.findAllByIds(products);
+
+    if (!existsProducts.length) {
+      throw new AppError('Could not find any products with this ids.');
+    }
+
+    const existsProductsIds = existsProducts.map(product => product.id);
+
+    const checkInexistentProducts = products.filter(
+      product => !existsProductsIds.includes(product.id)
+    );
+
+    if (checkInexistentProducts.length) {
+      throw new AppError(
+        `Could not find product ${checkInexistentProducts[0].id}`
+      );
+    }
+
+    const quantityAvailable = products.filter(
+      product =>
+        existsProducts.filter(p => p.id === product.id)[0].quantity <
+        product.quantity
+    );
+
+    if (quantityAvailable.length) {
+      throw new AppError(
+        `The quantity ${quantityAvailable[0].quantity} is not available for ${quantityAvailable[0].id}`
+      );
+    }
+
+    const serializedProducts = products.map(product => ({
+      product_id: product.id,
+      quantity: product.quantity,
+      price: existsProducts.filter(p => p.id === product.id)[0].price
+    }));
+
+    const order = await ordersRepository.createOrder({
+      customer: customerExists,
+      products: serializedProducts
+    });
+
+    if (!order) {
+      throw new AppError('Failed in create order.');
+    }
+
+    const { order_products } = order;
+
+    const updatedProductQuantity = order_products.map(product => ({
+      id: product.product_id,
+      quantity:
+        existsProducts.filter(p => p.id === product.id)[0].quantity -
+        product.quantity
+    }));
+
+    await productsRepository.save(updatedProductQuantity);
+
+    return order;
+  }
+}
